f3e13b6cde7f0e251e68ef9ede1527913d4bc23f
Arjan <ajodedra@gmail.com>
2018-08-13 08:54:14 +0530

    another changes

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,8 +1,9 @@
-import { Component } from "../../../node_modules/@angular/core";
+import { Component , OnInit } from "../../../node_modules/@angular/core";
 
 @Component({
     selector:'app-server',
-    templateUrl:'./server.component.html'
+    templateUrl:'./server.component.html',
+    styleUrls:['./server.component.css']
 })
 export class ServerComponent {
 

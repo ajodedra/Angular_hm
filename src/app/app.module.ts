55e81a01484b5dd8f443101f86e240566f0bed6e
Arjan <ajodedra@gmail.com>
2018-08-14 09:37:58 +0530

    warning and success msg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule} from '@angular/forms';
 import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component'
+import { ServersComponent } from './servers/servers.component';
+import { SuccessComponent } from './success/success.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/warning/warning.component.ts
@@ -0,0 +1,10 @@
+import {Component} from  "../../../node_modules/@angular/core";
+
+@Component({
+   selector:'app-warning',
+   templateUrl:'./warning.component.html',
+   styleUrls:['./warning.component.css']
+})
+export class WarningComponent{
+
+}

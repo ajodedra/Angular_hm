367b6116ec37c0c96283b67a87d281070474bb00
Arjan <ajodedra@gmail.com>
2018-08-14 09:46:20 +0530

    warning/success done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule} from '@angular/forms';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { SuccessComponent } from './success/success.component'
+import { WarningComponent } from './warning/warning.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
+    WarningComponent,
     SuccessComponent
   ],
   imports: [
